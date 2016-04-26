57c9ab44cc973b48afce9700e0165b47e597dc51
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-04-26 15:09:51 +0100

    Remove views module

--- betterinformatics/__init__.py
+++ betterinformatics/__init__.py
@@ -1,7 +1,4 @@
 from flask import Flask
 app = Flask(__name__)
 
-import betterinformatics.views
 from betterinformatics.bi import BI
-
-
--- betterinformatics/views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, redirect
-from flask import Markup
-import markdown
-
-from betterinformatics import app
-
-@app.route('/')
-def index():
-    return redirect('/home')
