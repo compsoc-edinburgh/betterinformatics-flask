bfb6b5586aec1924ab47c01c0543a9cf32c65c07
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-05-15 16:14:16 +0100

    Add login page backend with small cookie

--- betterinformatics/bi.py
+++ betterinformatics/bi.py
@@ -2,6 +2,7 @@ import yaml
 import os.path
 
 from flask import redirect, render_template, request, url_for
+from flask import flash, make_response
 
 from betterinformatics.page import Page
 
@@ -42,11 +43,16 @@ class BI(object):
 
         self.history_file = config["history"]["file"]
         self.history_path = config["history"]["folder"]
-        assert(os.path.exists(self.history_path))  # TODO add err msg
+
+        # pls make it
+        assert(os.path.exists(self.history_path))
 
         print(config["pages"])
         self.page_names = config["pages"]
         self.index_page = config["general"]["index"]
+
+        self.question = config["sign-in"]["question"]
+        self.password = config["sign-in"]["password"]
 
     def gen_views(self):
         self.app.add_url_rule("/", 'index', self.index)
@@ -61,7 +67,8 @@ class BI(object):
             p = Page(name, path, history_path, history_file)
             p.load_content()
             self.pages[name] = p
-            self.app.add_url_rule('/pages/<page>', methods=['get', 'post'],
+            self.app.add_url_rule('/pages/<page>',
+                                  methods=['get', 'post'],
                                   view_func=self.bi_page)
             self.app.add_url_rule('/pages/<page>/edit',
                                   methods=['get', 'post'],
@@ -69,23 +76,39 @@ class BI(object):
             self.app.add_url_rule('/pages/<page>/edit/publish',
                                   methods=['get', 'post'],
                                   view_func=self.publish_page)
+        self.app.add_url_rule('/login', view_func=self.login)
+        self.app.add_url_rule('/login/check', methods=['post'],
+                              view_func=self.login_check)
 
     def bi_page(self, page):
+        if not self.check_cookie():
+            return redirect("/login")
         p = self.pages[page]
         name = p.get_name()
         content = p.get_content()
+
         return render_template("page.html", name=name, content=content,
                                pages=self.page_names)
 
     def edit_page(self, page):
+        if not self.check_cookie():
+            return redirect("/login")
         p = self.pages[page]
         name = p.get_name()
         revision = p.get_revision()
         content = p.get_md()
-        return render_template("edit.html", name=name, content=content,
-                               pages=self.page_names, revision=revision)
+        logged = request.cookies.get("logged")
+
+        if logged == "logged":
+            return render_template("edit.html", name=name, content=content,
+                                   pages=self.page_names, revision=revision)
+        else:
+            return redirect("/login")
 
     def publish_page(self, page):
+        if not self.check_cookie():
+            return redirect("/login")
+
         # get data
         text = request.form['textarea']
         p = self.pages[page]
@@ -93,9 +116,29 @@ class BI(object):
         p.write_md()
         return redirect(url_for("bi_page", page=page))
 
+    def login(self):
+        return render_template("signin.html", name="Sign in",
+                               question=self.question)
+
+    def login_check(self):
+        psswd = request.form["password"]
+        if psswd == self.password:
+            resp = make_response(redirect("/"))
+            resp.set_cookie("logged", "logged")
+            return resp
+        else:
+            flash("Wrong answer!")
+            return redirect(url_for("login"))
+
     def index(self):
+        if not self.check_cookie():
+            return redirect("/login")
         return redirect("/pages/" + self.index_page)
 
     def run(self):
         """Runs the Flask application"""
         self.app.run(host=self.host, debug=self.debug)
+
+    def check_cookie(self):
+        value = request.cookies.get("logged")
+        return value == "logged"
